3a311a981096be8c25b26d58c255151489965289
Kamil Bednarczyk <genezi333@poczta.fm>
2018-10-23 15:53:17 +0200

    implement inventory without last special function

--- controller/inventory_controller.py
+++ controller/inventory_controller.py
@@ -3,6 +3,7 @@ from view import terminal_view
 from model.inventory import inventory
 from controller import common
 
+
 def run():
     """
     Starts this module and displays its menu.
@@ -13,4 +14,36 @@ def run():
         None
     """
 
-    # your code
+    options = ["Show inventory",
+               "Add",
+               "Update",
+               "Delete",
+               "Available items",
+               "Average durability"]
+
+    table = inventory.read_inventory_data()
+    choice = None
+    while choice != "0":
+        choice = terminal_view.get_submenu_choice(options)
+        if choice == "1":
+            terminal_view.print_table(table, ["Id", "Name", "Manufacturer", "Purchase year", "Durability"])
+        elif choice == "2":
+            inputs = terminal_view.get_inputs(["Name", "Manufacturer", "Purchase year",
+                                               "Durability"], "Add new item to inventory:")
+            new_record = inventory.add_id(table, inputs)
+            table = inventory.add(table, new_record)
+        elif choice == "3":
+            id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to edit:")[0]
+            inputs = terminal_view.get_inputs(["Name", "Manufacturer", "Purchase year", "Durability"], "Edit Fields")
+            table = inventory.update(table, id_, inputs)
+        elif choice == "4":
+            id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to edit:")[0]
+            table = inventory.remove(table, id_)
+        elif choice == "5":
+            available_items = inventory.get_available_items(table)
+            terminal_view.print_result(available_items, "Available items: ")
+        elif choice == "6":
+            durability_averages = inventory.get_average_durability_by_manufacturers(table)
+            terminal_view.print_result(durability_averages, "Average durability by manufacturer: ")
+        else:
+            terminal_view.print_error_message("There is no such choice.")
--- model/inventory/inventory.py
+++ model/inventory/inventory.py
@@ -25,7 +25,7 @@ def add(table, record):
     Returns:
         list: Table with a new record
     """
-    # your code
+    table.append(record)
 
     return table
 
@@ -42,7 +42,10 @@ def remove(table, id_):
         list: Table without specified record.
     """
 
-    # your code
+    for i, record in enumerate(table):
+        if record[0] == id_:
+            table.pop(i)
+            break
 
     return table
 
@@ -60,7 +63,12 @@ def update(table, id_, record):
         list: table with updated record
     """
 
-    # your code
+    for i, old_record in enumerate(table):
+        if old_record[0] == id_:
+            new_record = [id_] + record
+            fill_blanks(new_record, old_record)
+            table[i] = new_record
+            break
 
     return table
 
@@ -78,8 +86,18 @@ def get_available_items(table):
     Returns:
         list: list of lists (the inner list contains the whole row with their actual data types)
     """
+    year_index = 3
+    durability_index = 4
+    current_year = 2015
+    available_items = []
+    for record in table:
+        record_year = int(record[year_index])
+        record_durability = int(record[durability_index])
+        validity_date = record_year + record_durability
+        if validity_date >= current_year:
+            available_items.append(record)
 
-    # your code
+    return available_items
 
 
 def get_average_durability_by_manufacturers(table):
@@ -92,5 +110,47 @@ def get_average_durability_by_manufacturers(table):
     Returns:
         dict: a dictionary with this structure: { [manufacturer] : [avg] }
     """
+    manufacturer_index = 2
+    durability_index = 4
+    durability_times = {}
+    durability_averages = {}
+    for record in table:
+        durability = int(record[durability_index])
+        print(durability)
+        manufacturer = record[manufacturer_index]
+        print(durability_times)
+        durability_times[manufacturer] = durability_times.get(manufacturer, list()).append(durability)
 
-    # your code
+    for manufacturer in durability_times:
+        durability_averages[manufacturer] = get_average(durability_times[manufacturer])
+
+    return durability_averages
+
+
+# add to commons
+def get_average(numbers: list) -> float:
+    summed = 0
+    for number in numbers:
+        summed += number
+
+    average = float(summed / len(numbers))
+    return average
+
+
+def read_inventory_data():
+    table = data_manager.get_table_from_file("model/inventory/inventory.csv")
+    return table
+
+
+def add_id(table, inputs):
+    '''Creates new id and add it to incomplete record(inputs)'''
+    new_id = common.generate_random(table)
+    record = [new_id] + inputs
+    return record
+
+
+def fill_blanks(new, old):
+    '''updates blank places in new with old info'''
+    for i in range(len(old)):
+        if not new[i]:
+            new[i] = old[i]
