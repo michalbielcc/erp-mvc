767a0dfccbf2cb408e84cd1e39721e9a4d129363
Kamil Bednarczyk <genezi333@poczta.fm>
2018-10-24 13:02:28 +0200

    crm implementation

--- controller/crm_controller.py
+++ controller/crm_controller.py
@@ -3,6 +3,7 @@ from view import terminal_view
 from model.crm import crm
 from controller import common
 
+
 def run():
     """
     Starts this module and displays its menu.
@@ -13,4 +14,35 @@ def run():
         None
     """
 
-    # your code
+    options = ["Show crm",
+               "Add",
+               "Update",
+               "Delete",
+               "Longest name id",
+               "Subscribers"]
+
+    table = crm.read_crm_data()
+    choice = None
+    while choice != "0":
+        choice = terminal_view.get_submenu_choice(options)
+        if choice == "1":
+            terminal_view.print_table(table, ["Id", "Name", "Email", "Subscribed"])
+        elif choice == "2":
+            inputs = terminal_view.get_inputs(["Name", "Email", "Subscribed"], "Add new item to crm:")
+            new_record = crm.add_id(table, inputs)
+            table = crm.add(table, new_record)
+        elif choice == "3":
+            id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to edit:")[0]
+            inputs = terminal_view.get_inputs(["Name", "Email", "Subscribed"], "Edit Fields: ")
+            table = crm.update(table, id_, inputs)
+        elif choice == "4":
+            id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to edit:")[0]
+            table = crm.remove(table, id_)
+        elif choice == "5":
+            longest_name_id = crm.get_longest_name_id(table)
+            terminal_view.print_result(longest_name_id, "Longest name id: ")
+        elif choice == "6":
+            subscribers = crm.get_subscribed_emails(table)
+            terminal_view.print_result(subscribers, "Subscribers: ")
+        else:
+            terminal_view.print_error_message("There is no such choice.")
