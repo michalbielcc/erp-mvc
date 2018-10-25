cf4eb46ab08a6a94628f6ed27a4f27040e800029
Kamil Bednarczyk <genezi333@poczta.fm>
2018-10-25 11:07:19 +0200

    add check_input function

--- controller/common.py
+++ controller/common.py
@@ -1,3 +1,11 @@
 """ Common functions for controllers
 implement commonly used functions here
 """
+check_input(inputs, types):
+    '''Checks if inputs have expected format'''
+    for i in range(len(inputs)):
+        try:
+            types[i](inputs[i])
+        except ValueError:
+            return False
+    return True
