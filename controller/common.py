f8376cf5115c963602da35412ab9ca3b49524448
Kamil Bednarczyk <genezi333@poczta.fm>
2018-10-25 11:20:24 +0200

    add check_id_presence function

--- controller/common.py
+++ controller/common.py
@@ -1,6 +1,8 @@
 """ Common functions for controllers
 implement commonly used functions here
 """
+
+
 def check_input(inputs, types):
     '''Checks if inputs have expected format'''
     for i in range(len(inputs)):
@@ -9,3 +11,13 @@ def check_input(inputs, types):
         except ValueError:
             return False
     return True
+
+
+def check_id_presence(table, id_):
+    id_index = 0
+    is_in = False
+    for record in table:
+        if record[id_index] == id_:
+            is_in = True
+
+    return is_in
