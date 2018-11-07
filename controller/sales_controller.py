4b3591313601f09c68562597299fbc8daec64063
Kamil Bednarczyk <genezi333@poczta.fm>
2018-11-07 15:09:32 +0100

    refactor sales manager

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -35,60 +35,70 @@ def run():
     while choice != "0":
         choice = terminal_view.get_submenu_choice(options, "Sales menu")
         if choice == "1":
-            terminal_view.print_table(table, ["Id", "Title", "Price", "Month", "Day", "Year", "Customer Id"])
+            show_inventory(table)
         elif choice == "2":
-            inputs = terminal_view.get_inputs(["Title", "Price", "Month", "Day", "Year",
-                                               "Customer Id"], "Add new item to sales:")
-            types = [str, int, int, int, int, str]
-            if common.check_input(inputs, types) and check_fields(inputs):
-                new_record = sales.add_id(table, inputs)
-                table = sales.add(table, new_record)
-                common.export_to_file(table, 'model/sales/sales.csv')
+            add_new_record(table)
         elif choice == "3":
-            id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to edit:")[0]
-            inputs = terminal_view.get_inputs(
-                ["Title", "Price", "Month", "Day", "Year", "Customer Id"], "Edit Fields: ")
-            types = [str, int, int, int, int, str]
-            if common.check_input(inputs, types) and check_fields(inputs):
-                table = sales.update(table, id_, inputs)
-                common.export_to_file(table, 'model/sales/sales.csv')
+            edit_record(table)
         elif choice == "4":
-            id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to delete:")[0]
-            table = sales.remove(table, id_)
-            common.export_to_file(table, 'model/sales/sales.csv')
+            delete_record(table)
         elif choice == "5":
-            cheapest_items_id = sales.get_lowest_price_item_id(table)
-            terminal_view.print_result(cheapest_items_id, "Cheapest item's id: ")
+            show_cheapest_id(table)
         elif choice == "6":
             sold_between(table)
         elif choice == '7':
-            id_ = terminal_view.get_inputs(["Id"], "Enter id of the record: ")[0]
-            item_title = sales.get_title_by_id_from_table(table, id_)
-            terminal_view.print_result(item_title, f"Title of game with id {id_}:")
+            game_titles_by_id(table)
         elif choice == '8':
-            sold_last_id = sales.get_item_id_sold_last_from_table(table)
-            terminal_view.print_result(sold_last_id, "Id of game sold last: ")
+            last_sold_game_id(table)
         elif choice == '9':
-            sold_last_title = sales.get_item_title_sold_last_from_table(table)
-            terminal_view.print_result(sold_last_title, "Title of game sold last: ")
+            last_sold_game_title(table)
         elif choice == '10':
-            # get_the_sum_of_prices_from_table(table, item_ids)
-            pass
+            sum_price_by_id(table)
         elif choice == '11':
-            sale_id = terminal_view.get_inputs(["Id"], "Enter id of the record: ")[0]
-            customer_id = sales.get_customer_id_by_sale_id_from_table(table, sale_id)
-            terminal_view.print_result(customer_id, "Customer id: ")
+            cusomer_id_by_sale_id(table)
         elif choice == '12':
-            all_customer_ids = sales.get_all_customer_ids_from_table(table)
-            terminal_view.print_result(all_customer_ids, "All customer id's: ")
+            show_all_customers_ids(table)
         elif choice == '13':
-            all_sales_ids = sales.get_all_sales_ids_for_customer_ids_form_table(table)
-            terminal_view.print_result(all_sales_ids, "All sales id's by customer id's: ")
+            sale_ids_by_customer_ids(table)
         elif choice == '14':
-            num_of_sales = sales.get_num_of_sales_per_customer_ids_from_table(table)
-            terminal_view.print_result(num_of_sales, "Number of sales per customer id: ")
+            number_of_sales_per_customer(table)
         elif choice != '0':
             terminal_view.print_error_message("There is no such choice.")
+
+
+def show_inventory(table):
+    terminal_view.print_table(table, ["Id", "Title", "Price", "Month", "Day", "Year", "Customer Id"])
+
+
+def add_new_record(table):
+    inputs = terminal_view.get_inputs(["Title", "Price", "Month", "Day", "Year",
+                                       "Customer Id"], "Add new item to sales:")
+    types = [str, int, int, int, int, str]
+    if common.check_input(inputs, types) and check_fields(inputs):
+        new_record = sales.add_id(table, inputs)
+        table = sales.add(table, new_record)
+        common.export_to_file(table, 'model/sales/sales.csv')
+
+
+def edit_record(table):
+    id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to edit:")[0]
+    inputs = terminal_view.get_inputs(
+        ["Title", "Price", "Month", "Day", "Year", "Customer Id"], "Edit Fields: ")
+    types = [str, int, int, int, int, str]
+    if common.check_input(inputs, types) and check_fields(inputs):
+        table = sales.update(table, id_, inputs)
+        common.export_to_file(table, 'model/sales/sales.csv')
+
+
+def delete_record(table):
+    id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to delete:")[0]
+    table = sales.remove(table, id_)
+    common.export_to_file(table, 'model/sales/sales.csv')
+
+
+def show_cheapest_id(table):
+    cheapest_items_id = sales.get_lowest_price_item_id(table)
+    terminal_view.print_result(cheapest_items_id, "Cheapest item's id: ")
 
 
 def sold_between(table):
@@ -114,6 +124,48 @@ def sold_between(table):
     else:
         sold_items = sales.get_items_sold_between(table, *inputs)
         terminal_view.print_result(sold_items, "Sold items: ")
+
+
+def game_titles_by_id(table):
+    id_ = terminal_view.get_inputs(["Id"], "Enter id of the record: ")[0]
+    item_title = sales.get_title_by_id_from_table(table, id_)
+    terminal_view.print_result(item_title, f"Title of game with id {id_}:")
+
+
+def last_sold_game_id(table):
+    sold_last_id = sales.get_item_id_sold_last_from_table(table)
+    terminal_view.print_result(sold_last_id, "Id of game sold last: ")
+
+
+def last_sold_game_title(table):
+    sold_last_title = sales.get_item_title_sold_last_from_table(table)
+    terminal_view.print_result(sold_last_title, "Title of game sold last: ")
+
+
+def sum_price_by_id(table):
+    # get_the_sum_of_prices_from_table(table, item_ids)
+    pass
+
+
+def cusomer_id_by_sale_id(table):
+    sale_id = terminal_view.get_inputs(["Id"], "Enter id of the record: ")[0]
+    customer_id = sales.get_customer_id_by_sale_id_from_table(table, sale_id)
+    terminal_view.print_result(customer_id, "Customer id: ")
+
+
+def show_all_customers_ids(table):
+    all_customer_ids = sales.get_all_customer_ids_from_table(table)
+    terminal_view.print_result(all_customer_ids, "All customer id's: ")
+
+
+def sale_ids_by_customer_ids(table):
+    all_sales_ids = sales.get_all_sales_ids_for_customer_ids_form_table(table)
+    terminal_view.print_result(all_sales_ids, "All sales id's by customer id's: ")
+
+
+def number_of_sales_per_customer(table):
+    num_of_sales = sales.get_num_of_sales_per_customer_ids_from_table(table)
+    terminal_view.print_result(num_of_sales, "Number of sales per customer id: ")
 
 
 def check_fields(inputs):
