baec4c113b8bb5f4fdf137abb22102af0ece6e5a
Kamil Bednarczyk <genezi333@poczta.fm>
2018-11-07 14:18:47 +0100

    Refactor accounting controller

--- controller/accounting_controller.py
+++ controller/accounting_controller.py
@@ -26,40 +26,60 @@ def run():
     while choice != "0":
         choice = terminal_view.get_submenu_choice(options, "Accounting menu")
         if choice == "1":
-            terminal_view.print_table(table, ["Id", "Month", "Day", "Year", "Type", "Amount"])
+            show_accounting(table)
         elif choice == "2":
-            inputs = terminal_view.get_inputs(
-                ["Month", "Day", "Year", "Type", "Amount"], "Add new record to accounting:")
-            types = [int, int, int, str, int]
-            if common.check_input(inputs, types) and check_fields(inputs):
-                new_record = accounting.add_id(table, inputs)
-                table = accounting.add(table, new_record)
-                common.export_to_file(table, 'model/accounting/items.csv')
+            add_new_record(table)
         elif choice == "3":
-            id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to edit:")[0]
-            inputs = terminal_view.get_inputs(["Month", "Day", "Year", "Type", "Amount"], "Edit Fields")
-            types = [int, int, int, str, int]
-            if common.check_input(inputs, types) and check_fields(inputs):
-                table = accounting.update(table, id_, inputs)
-                common.export_to_file(table, 'model/accounting/items.csv')
+            edit_record(table)
         elif choice == "4":
-            id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to delete:")[0]
-            table = accounting.remove(table, id_)
-            common.export_to_file(table, 'model/accounting/items.csv')
+            delete_record(table)
         elif choice == "5":
-            highest_profit_year = accounting.which_year_max(table)
-            terminal_view.print_result(highest_profit_year, "Most profitable year: ")
+            most_profitable_year(table)
         elif choice == "6":
             average_profit_for_year(table)
         elif choice != '0':
             terminal_view.print_error_message("There is no such choice.")
 
 
+def show_accounting(table):
+    terminal_view.print_table(table, ["Id", "Month", "Day", "Year", "Type", "Amount"])
+
+
+def add_new_record(table):
+    inputs = terminal_view.get_inputs(
+        ["Month", "Day", "Year", "Type", "Amount"], "Add new record to accounting:")
+    types = [int, int, int, str, int]
+    if common.check_input(inputs, types) and check_fields(inputs):
+        new_record = accounting.add_id(table, inputs)
+        accounting.add(table, new_record)
+        common.export_to_file(table, 'model/accounting/items.csv')
+
+
+def edit_record(table):
+    id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to edit:")[0]
+    inputs = terminal_view.get_inputs(["Month", "Day", "Year", "Type", "Amount"], "Edit Fields")
+    types = [int, int, int, str, int]
+    if common.check_input(inputs, types) and check_fields(inputs):
+        accounting.update(table, id_, inputs)
+        common.export_to_file(table, 'model/accounting/items.csv')
+
+
+def delete_record(table):
+    id_ = terminal_view.get_inputs(["Id"], "Enter id of the record you want to delete:")[0]
+    accounting.remove(table, id_)
+    common.export_to_file(table, 'model/accounting/items.csv')
+
+
+def most_profitable_year(table):
+    highest_profit_year = accounting.which_year_max(table)
+    terminal_view.print_result(highest_profit_year, "Most profitable year: ")
+
+
 def average_profit_for_year(table):
     try:
         year = terminal_view.get_inputs(["Year"], "Enter a year:")[0]
         common.check_year(year)
-        average_profit = accounting.avg_amount(table, year)
+        average_profit = accounting.avg_amount(table, int(year))
     except ZeroDivisionError:
         terminal_view.print_error_message("There is no such year")
     except ValueError as e:
