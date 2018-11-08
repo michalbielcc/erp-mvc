97bd9c662f5a224a60e9632a83377cda6ad61447
Michal Biel <mierwew@gmail.com>
2018-11-08 12:59:35 +0100

    closing animation implemented

--- controller/root_controller.py
+++ controller/root_controller.py
@@ -1,4 +1,4 @@
-# everything you'll need is imported:
+from __future__ import division
 from view import terminal_view
 from controller import store_controller
 from controller import hr_controller
@@ -8,6 +8,14 @@ from controller import sales_controller
 from controller import crm_controller
 from controller import common
 from controller import data_analyser_controller
+from pyfiglet import Figlet
+from asciimatics.effects import Scroll, Mirage, Wipe, Cycle, Matrix, BannerText, Stars, Print
+from asciimatics.particles import DropScreen
+from asciimatics.renderers import FigletText, SpeechBubble, Rainbow, Fire
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+from asciimatics.exceptions import ResizeScreenError
+import sys
 
 
 def run():
@@ -37,4 +45,51 @@ def run():
         elif choice == '7':
             data_analyser_controller.run()
         elif choice == '0':
-            terminal_view.print_error_message("Good Bye")
+            try:
+                Screen.wrapper(_credits)
+                sys.exit(0)
+            except ResizeScreenError:
+                pass
+
+
+def _credits(screen):
+    scenes = []
+
+    text = Figlet(font="banner", width=200).renderText("ERP PROJECT")
+    width = max([len(x) for x in text.split("\n")])
+
+    effects = [
+        BannerText(
+            screen,
+            Rainbow(screen, FigletText(
+                "THANKS FOR USING OUR SOFT!", font='slant')),
+            screen.height // 2 - 3,
+            Screen.COLOUR_GREEN)
+    ]
+    scenes.append(Scene(effects))
+
+    effects = [
+        Print(screen,
+              Fire(screen.height, 80, text, 0.4, 40, screen.colours),
+              0,
+              speed=1,
+              transparent=False),
+        Print(screen,
+              FigletText("Kamil & Michal", "banner"),
+              screen.height - 9, x=(screen.width - width) // 2 + 1,
+              colour=Screen.COLOUR_BLACK,
+              bg=Screen.COLOUR_BLACK,
+              speed=1),
+        Print(screen,
+              FigletText("Kamil & Michal", "banner"),
+              screen.height - 9,
+              colour=Screen.COLOUR_WHITE,
+              bg=Screen.COLOUR_WHITE,
+              speed=1),
+    ]
+    scenes.append(Scene(effects, 100))
+    
+    for i in range(100):
+        scenes.append(Scene(effects))
+
+    screen.play(scenes, stop_on_resize=True)
